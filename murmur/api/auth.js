95a397143bebb5758814b9a237c84499cd4ad56e
vijay <42271835+VijayPutta@users.noreply.github.com>
2024-07-15 19:56:40 +0000

    server end is completed

--- murmur/api/auth.js
+++ murmur/api/auth.js
@@ -77,7 +77,9 @@ router.post('/',
 router.get('/',auth, async (req,res) => 
     {
         try{
-            const user =  await User.findById(req.user.id).select('-password');
+            const user =  await User.findById(req.user.id)
+            // commented for now, need password to be hided
+            //.select('-password');
             res.json(user);
         }catch(err){
             console.error(err.message);
